f46e9cd9f66700e5fbe878ec174a14fe83e526e2
yxh1126 <yuxianhongtw@gmail.com>
2019-10-25 10:25:40 +0800

    add ConsoleApp

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Group g1 = new Group();
+            //g1.Name = "甲組";
+
+            Group g2 = new Group();
+            //g2.Name = "乙組";
+
+            Group g3 = new Group();
+            //g3.Name = "丙組";
+            g1.Students.Add(new Student());
+
+            g1.ShowStudentCount();
+            g2.ShowStudentCount();
+            g3.ShowStudentCount();
+
+            Group.UnitName = "電機系";
+            g1.ShowStudentCount();
+            g2.ShowStudentCount();
+            g3.ShowStudentCount();
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Group
+    {
+        public static string UnitName = "電子系";
+
+        public Group()
+        {
+            this.Students = new List<Student>();
+        }
+
+        public string Name;
+        public List<Student> Students;
+
+
+        public void ShowStudentCount()
+        {
+            Console.WriteLine(UnitName + Students.Count);
+        }
+    }
+
+    public class Student
+    {
+        public string Name;
+    }
+}
